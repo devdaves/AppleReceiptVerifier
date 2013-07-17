f0404fc8b7cdb3d3a4c2ef5d284a3ee921a10141
Dave Stensgaard <info@devdave.com>
2013-07-16 21:26:41 -0700

    Add in test pages.

--- src/AppleReceiptVerifier.Web/Controllers/HomeController.cs
+++ src/AppleReceiptVerifier.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppleReceiptVerifier.Web.Models;
+using Newtonsoft.Json;
 
 namespace AppleReceiptVerifier.Web.Controllers
 {
@@ -17,7 +19,30 @@ namespace AppleReceiptVerifier.Web.Controllers
         /// <returns>returns ActionResult</returns>
         public ActionResult Index()
         {
-            return new EmptyResult();
+            ReceiptTestViewModel model = new ReceiptTestViewModel();
+            model.Environment = "production";
+            return this.View(model);
+        }
+
+        /// <summary>
+        /// Indexes the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>returns ActionResult</returns>
+        [HttpPost]
+        public ActionResult Index(ReceiptTestViewModel model)
+        {
+            ReceiptManager receiptManager = new ReceiptManager();
+            var env = AppleReceiptVerifier.Environments.Production;
+            if (model.Environment == "sandbox")
+            {
+                env = AppleReceiptVerifier.Environments.Sandbox;
+            }
+
+            var response = receiptManager.ValidateReceipt(env, model.ReceiptData);
+            model.ReceiptResponse = response;
+
+            return this.View(model);
         }
     }
 }
---
+++ src/AppleReceiptVerifier.Web/Models/ReceiptTestViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppleReceiptVerifier.Web.Models
+{
+    /// <summary>
+    /// Receipt Test View Model
+    /// </summary>
+    public class ReceiptTestViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReceiptTestViewModel"/> class.
+        /// </summary>
+        public ReceiptTestViewModel()
+        {
+            this.Environments = new List<SelectListItem>()
+                {
+                    new SelectListItem { Selected = false, Text = "Apple Production", Value = "production" },
+                    new SelectListItem { Selected = false, Text = "Apple Sandbox", Value = "sandbox" },
+                };
+        }
+
+        /// <summary>
+        /// Gets or sets the environment.
+        /// </summary>
+        /// <value>
+        /// The environment.
+        /// </value>
+        [DisplayName("Environment")]
+        public string Environment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the environments.
+        /// </summary>
+        /// <value>
+        /// The environments.
+        /// </value>
+        public List<SelectListItem> Environments { get; set; }
+
+        /// <summary>
+        /// Gets or sets the receipt data.
+        /// </summary>
+        /// <value>
+        /// The receipt data.
+        /// </value>
+        [DisplayName("Receipt Data")]
+        public string ReceiptData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the receipt response.
+        /// </summary>
+        /// <value>
+        /// The receipt response.
+        /// </value>
+        public AppleReceiptVerifier.Models.Response ReceiptResponse { get; set; }
+    }
+}
